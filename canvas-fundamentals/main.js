6656d418cb67fc1bfbc5a13082272a71b43d00a6
Daniel Kariuki <freakoutbond2@gmail.com>
2022-11-01 12:56:41 +0300

    particle changing color

--- canvas-fundamentals/main.js
+++ canvas-fundamentals/main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let particlesArray = [];
+let hue = 0;
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
@@ -26,7 +27,7 @@ class Particle {
         this.y = mouse.y;
        // this.x = Math.random() * canvas.width;
         //this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.size = Math.random() * 10 + 1;
         this.speedX = Math.random() * 3 -1.5;
         this.speedY = Math.random() * 3 -1.5;
     }
@@ -35,7 +36,7 @@ class Particle {
         this.y += this.speedY;
     }
     draw() {
-        ctx.fillStyle = 'white';
+        ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
@@ -56,8 +57,10 @@ function handleParticles() {
     for(let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
+        particlesArray[i].size-=0.02;
+        console.log(particlesArray[i].size)
         //remove particles from array that are smaller than 0.3
-        if(particlesArray[i].size < .3 ) {
+        if(particlesArray[i].size < 1 ) {
             particlesArray.splice(i, 1);
             console.log(particlesArray.length);
             i--;
@@ -68,7 +71,7 @@ function handleParticles() {
 window.addEventListener('click', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    for(let i = 0; i< 10; i++) {
+    for(let i = 0; i< 5; i++) {
         particlesArray.push(new Particle());
     }
 })
@@ -81,19 +84,21 @@ window.addEventListener('mousemove', function (event) {
     }
 })
 
-
+/*
 function drawCircle() {
     ctx.fillStyle = 'red';
     ctx.beginPath();
     ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
     ctx.fill();
 }
+*/
 
 function animate() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
     //clear rectangle to create trails
     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    hue++;
     handleParticles();
     requestAnimationFrame(animate);
 }
