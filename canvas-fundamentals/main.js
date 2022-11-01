4af764f7787de8da607e19dd206dce8cdfee785d
Daniel Kariuki <freakoutbond2@gmail.com>
2022-11-01 13:18:54 +0300

    particle with distinct color

--- canvas-fundamentals/main.js
+++ canvas-fundamentals/main.js
@@ -30,13 +30,14 @@ class Particle {
         this.size = Math.random() * 10 + 1;
         this.speedX = Math.random() * 3 -1.5;
         this.speedY = Math.random() * 3 -1.5;
+        this.color = 'hsl(' + hue + ', 100%, 50%)';
     }
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
     }
     draw() {
-        ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
