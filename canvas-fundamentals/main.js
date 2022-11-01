9f95f177f7af1789366210ab7f286842273b326e
Daniel Kariuki <freakoutbond2@gmail.com>
2022-11-01 12:13:28 +0300

    fixed particles generation point

--- canvas-fundamentals/main.js
+++ canvas-fundamentals/main.js
@@ -24,16 +24,16 @@ class Particle {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.size = Math.random() * 10 + 1;
         this.speedX = Math.random() * 3 -1.5;
         this.speedY = Math.random() * 3 -1.5;
     }
     update() {
-        this.x = this.speedX;
-        this.y = this.speedY;
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
     draw() {
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'blue';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
@@ -41,7 +41,7 @@ class Particle {
 }
 
 function init() {
-    for(let i = 0; i < 100; i++) {
+    for(let i = 0; i < 200; i++) {
         particlesArray.push(new Particle())
     }
 }
@@ -49,7 +49,10 @@ function init() {
 init();
 
 function handleParticles() {
-    
+    for(let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
 }
 
 window.addEventListener('click', function (event) {
@@ -61,7 +64,6 @@ window.addEventListener('click', function (event) {
 window.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    animate();
 })
 
 
@@ -74,6 +76,8 @@ function drawCircle() {
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCircle();
+    handleParticles();
     requestAnimationFrame(animate);
 }
+
+animate();
